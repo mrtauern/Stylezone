ddde0eaf0832ea0dc0258fec75d2135bc05d8b88
Gustav <Gustavrodsgaard@gmail.com>
2018-11-24 11:31:44 +0100

    commit before pulling master

--- src/main/java/com/stylezone/demo/controllers/BookingController.java
+++ src/main/java/com/stylezone/demo/controllers/BookingController.java
@@ -1,7 +1,13 @@
 package com.stylezone.demo.controllers;
 
+import com.stylezone.demo.services.BookingService;
+import com.stylezone.demo.services.BookingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BookingController {
+    @Autowired
+    BookingService bookingService;
+
 }
