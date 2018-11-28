8e726967b0dc500bc8d6db8dae7aa40a217575cf
hasan <hhaidari25@gmail.com>
2018-11-28 09:09:06 +0100

    commit to change branch

--- src/main/java/com/stylezone/demo/controllers/BookingController.java
+++ src/main/java/com/stylezone/demo/controllers/BookingController.java
@@ -5,9 +5,19 @@ import com.stylezone.demo.services.BookingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.util.logging.Logger;
+
 @Controller
 public class BookingController {
+
     @Autowired
     BookingService bookingService;
+
+
+    public BookingController() { }
+
+
+    Logger log = Logger.getLogger(BookingController.class.getName());
+
 
 }
