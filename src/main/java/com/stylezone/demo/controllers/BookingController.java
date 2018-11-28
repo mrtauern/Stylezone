df5b2e44fb23820ead4f4bb611818e8847fdd0d7
hasan <hhaidari25@gmail.com>
2018-11-28 12:09:10 +0100

    siden lavet

--- src/main/java/com/stylezone/demo/controllers/BookingController.java
+++ src/main/java/com/stylezone/demo/controllers/BookingController.java
@@ -4,7 +4,10 @@ import com.stylezone.demo.services.BookingService;
 import com.stylezone.demo.services.BookingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 @Controller
@@ -12,12 +15,27 @@ public class BookingController {
 
     @Autowired
     BookingService bookingService;
+    private String billedGalleri;
+    private String index;
 
 
     public BookingController() { }
 
-
     Logger log = Logger.getLogger(BookingController.class.getName());
 
+    @GetMapping("/")
+    public String index(Model model){
+        log.info("index called");
+
+        return index;        
+    }
+    
+    @GetMapping("/billedGalleri")
+    public String billedGalleri(Model model) {
+        log.info("billedGalleri called...");
+
+        return billedGalleri;
+
+    }
 
 }
